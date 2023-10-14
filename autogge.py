ac547c28deddb5898eaeeef4843f045d1664be32
tylerhilton3 <tyler.hilton@sbcglobal.net>
2023-10-13 21:45:54 -0600

    finished automatic coin farm for outer realms

--- autogge.py
+++ autogge.py
@@ -2,42 +2,42 @@ import pyautogui as p
 import time as t
 import random as r
 
-# var = bottom left corner coords, top right corner coords
-supply_bounds  = [(878, 1019),  (938, 926)]
-pillory_bounds = [(1050, 1020), (1111, 921)]
-location1      = [(1161, 653), (1161, 653)]
-location2      = [(1294, 721), (1294, 721)]
-skip_bounds    = [(1021, 684), (1062, 665)]
-sell1_bounds   = [(1161, 773), (1186, 748)]
-sell2_bounds   = [(1298, 837), (1315, 820)]
-confirm_bounds = [(1082, 773), (1182, 753)]
+# var = top left corner coords, bottom right corner coords
+supply_bounds  = [(873, 923),  (936, 1022)]
+# pillory_bounds = [(1050, 1020), (1111, 921)]
+location1      = (1178, 609)
+location1      = [location1, location1]
+location2      = (1291, 714)
+location2      = [location2, location2]
+skip_bounds    = [(1025, 666), (1063, 682)]
+sell1_bounds   = [(1180, 709), (1201, 729)]
+sell2_bounds   = [(1278, 809), (1301, 832)]
+confirm_bounds = [(1086, 754), (1188, 772)]
 
 
 
-def wait():
-    t.sleep(r.random()*0.5+0.7)
+def wait(sell):
+    t.sleep(r.random()*0.1+1+0.5*sell)
 
-def clik(coords):
-    wait()
-    x1 = coords[0][0]
-    x2 = coords[1][0]
-    y1 = coords[0][1]
-    y2 = coords[1][1]
+def clik(coords, sell=False):
+    wait(sell)
     xval = r.randint(coords[0][0], coords[1][0])
     yval = r.randint(coords[0][1], coords[1][1])
-    p.click(x=xval,y=yval)
+    p.moveTo(x=xval,y=yval)
+    t.sleep(0.1)
+    p.click()
 
 t.sleep(5)
 clik(supply_bounds)
 clik(location2)
-while p.pixel(524, 244) == (154, 178, 71):
+while p.pixel(60, 131) == (183, 0, 0):
     clik(supply_bounds)
     clik(location1)
     clik(skip_bounds)
-    clik(sell2_bounds)
+    clik(sell2_bounds,True)
     clik(confirm_bounds)
     clik(supply_bounds)
     clik(location2)
     clik(skip_bounds)
-    clik(sell1_bounds)
+    clik(sell1_bounds, True)
     clik(confirm_bounds)
